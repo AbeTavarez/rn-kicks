bb31513d78d6af17b85c334d12160b251cb93434
Abraham E. Tavarez <abetavarez@icloud.com>
2023-02-11 19:26:43 -0500

    product item component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ProductScreen from './src/screens/ProductScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ProductScreen />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
